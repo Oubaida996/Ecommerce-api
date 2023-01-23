569824ffa2334007febb6d1f023750ead8d9fbda
Obieda Akilan <obieda.jehad@gmail.com>
2023-01-23 08:48:10 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict';
+const express = require( 'express' );
+
+const app = express();
+
+
+app.get( '/', ( req, res ) => {
+    res.send( 'Home Page V1' );
+} )
+
+app.listen( 3000, () => {
+    console.log( 'Server running on port 3000' );
+} )
+
