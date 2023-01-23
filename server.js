93a84e40fd3be16807d41730f571aee46e21459d
Obieda Akilan <obieda.jehad@gmail.com>
2023-01-23 09:23:34 +0300

    install dotenv package and prepare the enviroment variables

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 'use strict';
 const express = require( 'express' );
+const dotenv = require( 'dotenv' );
+dotenv.config( { path: 'config.env' } );
+
 
 const app = express();
 
 
 app.get( '/', ( req, res ) => {
     res.send( 'Home Page v3' );
-} )
+} );
 
-app.listen( 3000, () => {
-    console.log( 'Server running on port 3000' );
+
+const PORT = process.env.PORT || 8000
+app.listen( PORT, () => {
+    console.log( `Server running on port  ${ PORT }` );
 } )
 
