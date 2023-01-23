7c71b1f9efcf53258b2d1713bbe73b3aef72cf8f
Obieda Akilan <obieda.jehad@gmail.com>
2023-01-23 10:45:28 +0300

    Install morgan package for logging our API requests

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 'use strict';
 const express = require( 'express' );
+const morgan = require( 'morgan' );
 const dotenv = require( 'dotenv' );
 dotenv.config( { path: 'config.env' } );
 
-
 const app = express();
 
+
+if ( process.env.NODE_ENV === 'development' ) {
+    app.use( morgan( 'tiny' ) );
+    console.log( ` Mode : ${ process.env.NODE_ENV } ` );
+}
 
 app.get( '/', ( req, res ) => {
     res.send( 'Home Page v3' );
