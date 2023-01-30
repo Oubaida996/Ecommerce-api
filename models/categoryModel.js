4ed9de700c7bd0e565539f3cdf1d2fa9f75af680
Obieda Akilan <obieda.jehad@gmail.com>
2023-01-30 09:42:56 +0300

    Prepare the schema of category model

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -3,7 +3,21 @@ const mongoose = require('mongoose');
 //=== 1- Create Schema
 
 const categorySchema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        required: [true, 'Category name is required'],
+        unique: [true, 'Category must be unique'],
+        maxLength: [32, 'Too long category name'],
+        minLength: [3, 'Too short category name'],
+    },
+
+    // A and B => shopping.com/a-and-b Ex: Healthy food =>shopping.com/healthy-and-food
+    slug: {
+        type: String,
+        lowercase: true,
+    },
+}, {
+    timestamps: true,
 });
 
 //==== 2- Create model
