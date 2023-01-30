f695a45e4d0264b7b2548ab5268a0bef54bdf1e4
Obieda Akilan <obieda.jehad@gmail.com>
2023-01-30 12:20:08 +0300

    Category Pagination

--- services/categoryService.js
+++ services/categoryService.js
@@ -6,19 +6,24 @@ const asyncHandler = require('express-async-handler');
 const categoryModel = require('../models/categoryModel');
 
 // @desc    Get a list of categories
-// @route   Post /api/v1/categories
-// @access    Public
+// @route   Get /api/v1/categories?page=<number>&limit=<number>
+// @access  Public
 exports.getCategories = asyncHandler(async(req, res) => {
-    const categories = await categoryModel.find({});
+    // (req.query.page * 1) means convert the string into integer number
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 5;
+    const skip = (page - 1) * limit;
+    const categories = await categoryModel.find({}).skip(skip).limit(limit);
     res.status(200).json({
         results: categories.length,
+        page,
         data: categories,
     });
 });
 
 // @desc    Create a category
 // @route   Post /api/v1/categories
-// @access    Private
+// @access  Private
 exports.createCategory = asyncHandler(async(req, res) => {
     const { name } = req.body;
     const category = await CategoryModel.create({ name, slug: slugify(name) });
