d0a37aafb48aa513e8237dea60b6e5283673b4ec
Obieda Akilan <obieda.jehad@gmail.com>
2023-02-12 15:06:42 +0300

    Added a new route to get specific subcategory

--- routes/subCategoryRoutes.js
+++ routes/subCategoryRoutes.js
@@ -2,9 +2,13 @@
 
 const express = require('express');
 const router = express.Router();
-const { createSubCategory } = require('../services/subCategoryService');
+const {
+  createSubCategory,
+  getSubCategory,
+} = require('../services/subCategoryService');
 
 router.route('/').post(createSubCategory);
 
+router.route('/:id').get(getSubCategory);
 
-module.exports =router;
+module.exports = router;
--- services/subCategoryService.js
+++ services/subCategoryService.js
@@ -3,21 +3,31 @@
 const SubCategoryModel = require('../models/subCategoryModel');
 const slugify = require('slugify');
 const asyncHandler = require('express-async-handler');
-const categoryModel = require('../models/categoryModel');
 const ApiError = require('../utils/ApiErorr');
 
 // @desc    Create subCategory
 // @route   POST /api/v1/subcategories
 // @access  Private
 exports.createSubCategory = asyncHandler(async (req, res, next) => {
-  const { category, name } = req.body;
+  const { categoryID, name } = req.body;
 
   const subCategory = await SubCategoryModel.create({
     name,
     slug: slugify(name),
-    category: category,
+    category: categoryID,
   });
   res.status(201).json({ data: subCategory });
 });
 
+// @desc    Get a specific subCategory depend on ID
+// @route   GET /api/v1/subcategories/:id
+// @params  id : integer
+// @access  Public
+exports.getSubCategory = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const subCategory = await SubCategoryModel.findById(id);
 
+  if (!subCategory)
+    return next(new ApiError(`The subcatgegory isn't exist`, 404));
+  res.status(200).json({ data: subCategory });
+});
