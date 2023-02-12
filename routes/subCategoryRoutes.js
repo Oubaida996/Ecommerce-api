6b04746bd373d0bca8a270cb1a6fbc721c78d9cf
Obieda Akilan <obieda.jehad@gmail.com>
2023-02-12 15:39:34 +0300

    Added an update_specific_subcategory route

--- routes/subCategoryRoutes.js
+++ routes/subCategoryRoutes.js
@@ -6,10 +6,11 @@ const {
   createSubCategory,
   getSubCategory,
   getSubCategories,
+  updateSubCategory,
 } = require('../services/subCategoryService');
 
 router.route('/').post(createSubCategory).get(getSubCategories);
 
-router.route('/:id').get(getSubCategory);
+router.route('/:id').get(getSubCategory).put(updateSubCategory);
 
 module.exports = router;
--- services/subCategoryService.js
+++ services/subCategoryService.js
@@ -47,3 +47,18 @@ exports.createSubCategory = asyncHandler(async (req, res, next) => {
   });
   res.status(201).json({ data: subCategory });
 });
+
+// @desc    Update subCategory
+// @route   POST /api/v1/subcategories
+// @access  Private
+exports.updateSubCategory = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const subCategory = await SubCategoryModel.findOneAndUpdate(
+    { _id: id },
+    { name, slug: slugify(name) },
+    { new: true }
+  );
+  res.status(200).json({ data: subCategory });
+});
